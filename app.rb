3af2911ced52ea5222d38cedc6f458e5b764000d
NicoH <n.hermet@gmail.com>
2022-04-22 16:14:05 +0200

    first commit

---
+++ app.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative './lib/board'
+require_relative './lib/game'
+require_relative './lib/player'
+require_relative './lib/position'
+require 'pry'
+
+Game.tictactoe
